77eb25b0627afef5166e2ef93b330756fdeba85f
minos95 <rahal.aminos@gmail.com>
2022-10-23 23:59:07 +0100

    update item and linkloverdishes

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -20,33 +20,35 @@ const Portfolio = () => {
     });
   };
   return (
-    <div id="portfolio" className="portfolio">
-      <h1 style={{ color: "#8d9ca6" }}>
-        <strong>My Best Work</strong>
-      </h1>
-      <div className="section">
-        <h2 className="section-title">Web Application</h2>
-        <div className="sub-section">{List(Listitems.itemWeb)}</div>
-      </div>
-      <div className="section">
-        <h2 className="section-title">Mobile Application</h2>
-        <div className="sub-section">{List(Listitems.itemMobileApp)}</div>
-      </div>
-      <div className="section">
-        <h2 className="section-title">Network</h2>
+    <>
+      <div id="portfolio" className="portfolio container">
+        <h1 style={{ color: "#8d9ca6" }}>
+          <strong>My Best Work</strong>
+        </h1>
+        <div className="section">
+          <h2 className="section-title">Web Application</h2>
+          <div className="sub-section">{List(Listitems.itemWeb)}</div>
+        </div>
+        <div className="section">
+          <h2 className="section-title">Mobile Application</h2>
+          <div className="sub-section">{List(Listitems.itemMobileApp)}</div>
+        </div>
+        <div className="section">
+          <h2 className="section-title">Network</h2>
 
-        <div className="sub-section">{List(Listitems.itemNetwork)}</div>
-      </div>
+          <div className="sub-section">{List(Listitems.itemNetwork)}</div>
+        </div>
 
-      <div className="section">
-        <h4 className="section-title">Games</h4>
-        <div className="sub-section">{List(Listitems.itemGame)}</div>
+        <div className="section">
+          <h4 className="section-title">Games</h4>
+          <div className="sub-section">{List(Listitems.itemGame)}</div>
+        </div>
       </div>
-      <div className="section">
+      <div className="section-certif">
         <h4 className="section-title">Certification</h4>
         <div className="sub-section">{List(Listitems.itemCertificat)}</div>
       </div>
-    </div>
+    </>
   );
 };
 
